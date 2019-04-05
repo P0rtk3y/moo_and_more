43d8c42c99f2a004d060c0bdce7546271db8ed63
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-04-05 04:32:55 +0000

    setup config and environment

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/moo_and_more/cli'
+require_relative '../lib/moo_and_more/lifeform'
+require_relative '../lib/moo_and_more/scraper'
+require_relative '../lib/moo_and_more/version'
