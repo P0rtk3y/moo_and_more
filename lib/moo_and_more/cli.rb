761dda2bcebc48c141657aa53fc74305cb3c9d97
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-04-05 20:55:16 +0000

    update load dependencies

--- lib/moo_and_more/cli.rb
+++ lib/moo_and_more/cli.rb
@@ -1,32 +1,36 @@
+#Our CLI Controller
+
 class MooAndMore::CLI
 
   def call
     puts "Welcome to Moo and More -- A Collective Noun Generator for Lifeforms!"
     # MooAndMore::Scraper.new.make_lifeform_lists
-    # start
+    start
   end
 
-  # def start
-  #   puts ""
-  #   puts "Which category would you like to see?\n
-  #        1. Mammals & Marsupials\n
-  #        2. Birds\n
-  #        3. Fish\n
-  #        4. Reptiles and Amphibians\n
-  #        5. Invertebrates (including insects, mollusks, etc.)\n
-  #        6. Single-Celled Organism\n
-  #        Enter (1 - 6)"
-  #   input = gets.strip.to_i
-  # 
+  def start
+    puts ""
+    puts <<-DOC
+    Which category would you like to see?
+         1. Mammals & Marsupials
+         2. Birds
+         3. Fish
+         4. Reptiles and Amphibians
+         5. Invertebrates (including insects, mollusks, etc.)
+         6. Single-Celled Organism
+         Enter: 1 - 6
+    DOC
+    input = gets.strip.to_i
+  end
   #   print_category(input)
-  # 
+  #
   #   puts ""
   #   puts "Type the number next to the lifeform to see its collective noun"
   #   input = gets.strip
-  # 
+  #
   #   lifeform = MooAndMore::Lifeform.find(input.to_i)
   #   print_lifeform(lifeform)
-  # 
+  #
   #   puts ""
   #   puts "Would you like to see another collective noun for a lifeform\n
   #        in the same category? (If yes, enter: Moove Over.\n
@@ -41,7 +45,7 @@ class MooAndMore::CLI
   #     "Thank you for visiting Moo and More!"
   #     exit
   #   end
-  # 
+  #
   # end
 
-end 
+end
