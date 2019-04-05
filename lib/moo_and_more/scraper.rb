d42ec19906319d2fb3ac190d79e72e58535f950f
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-04-05 04:27:09 +0000

    Nokogiri get_page

--- lib/moo_and_more/scraper.rb
+++ lib/moo_and_more/scraper.rb
@@ -1,2 +1,7 @@
-class Scraper
+class MooAndMore::Scraper
+
+  def get_page
+    Nokogiri::HTML(open("http://www.collectivenouns.biz/list-of-collective-nouns/collective-nouns-animals/"))
+  end
+
 end
